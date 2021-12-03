88f12cc18216cddaf7c6166394cee7acf50f7917
iuliabozzo <56926554+iuliabozzo@users.noreply.github.com>
2021-12-03 18:08:17 +0200

    FIRST si FOLLOW yuppiiiiii

--- LL1Parser.py
+++ LL1Parser.py
@@ -1,5 +1,5 @@
 from Grammar import Grammar
-
+from copy import deepcopy
 
 class LL1Parser:
 
@@ -8,61 +8,115 @@ class LL1Parser:
         # first and follow will be dictionaries that have as keys all non-terminals and as values list containing terminals corresponding to first respectively follow
         self.FIRST = {}
         self.FOLLOW = {}
-
-
-    def constructFirstForAlpha(self, remainingSymbols: list):
-        pass
-
+        self.constructFirst()
+        self.constructFollow()
 
     def constructFirst(self):
+        hasChanged = False
         for lhs in self.grammar.P.keys():
             self.FIRST[lhs] = set()
-            currentSet = set()
+            currentSet = deepcopy(self.FIRST[lhs])
             productions = self.grammar.P[lhs]
-            # print(productions)
             for production in productions:
                 productionList = production[0]
                 if productionList[0] in self.grammar.Sigma:
                     currentSet.add(productionList[0])
-            if currentSet != self.FIRST[lhs]:
-                self.FIRST[lhs] = self.FIRST[lhs].union(currentSet)
 
-        hasChangedMare = True
-        print(self.FIRST)
-        while hasChangedMare:
-            hasChangedMare = False
+            if len(self.FIRST[lhs]) != len(currentSet):
+                self.FIRST[lhs] = currentSet
+                hasChanged = True
+
+        while hasChanged:
+            hasChanged = False
             for lhs in self.grammar.P.keys():
+                currentSet = self.FIRST[lhs]
                 productions = self.grammar.P[lhs]
                 for production in productions:
-                    initialSet = self.FIRST[lhs]
                     productionList = production[0]
-                    hasChangedMic = False
-                    index = 0
-                    while index < len(productionList):
-                        if productionList[index] in self.grammar.N:
-                            self.FIRST[lhs] = self.FIRST[lhs].union(self.FIRST[productionList[index]])
-                            if 'eps' not in currentSet:
+
+                    copyCurrentSet = deepcopy(currentSet)
+                    for symbol in productionList:
+                        if symbol in self.grammar.N:
+                            for additional_symbol in self.FIRST[symbol]:
+                                if additional_symbol != 'eps':
+                                    copyCurrentSet.add(additional_symbol)
+                            if 'eps' not in self.FIRST[symbol]:
                                 break
-                            index += 1
-                    if initialSet != self.FIRST[lhs]:
-                        hasChangedMare = True
+                            else:
+                                if symbol != productionList[len(productionList) - 1]: # there are still some symbols in the productions to be checked
+                                    continue
+                                copyCurrentSet.add('eps')
+                                break
+                        else:
+                            copyCurrentSet.add(symbol)
+                            break
 
-                #     if productionList[0] in self.grammar.N:
-                #         self.FIRST[lhs] = self.FIRST[lhs].union(self.FIRST[productionList[0]])
-                #     while 'eps' in self.FIRST[lhs] and index < len(productionList):
-                #         self.FIRST[lhs].remove('eps')
-                #         self.FIRST[lhs] = self.FIRST[lhs].union(self.FIRST[productionList[index]])
-                #         index += 1
-                # hasChangedMare = False
+                    currentSet = currentSet.union(copyCurrentSet)
 
-        print(self.FIRST)
-
-
+                    if len(self.FIRST[lhs]) != len(currentSet):
+                        self.FIRST[lhs] = currentSet
+                        hasChanged = True
 
     def constructFollow(self):
-        pass
+        hasChanged = False
+        self.FOLLOW[self.grammar.S] = set("eps")
+
+        for lhs in self.grammar.P.keys():
+            self.FOLLOW[lhs] = set()
+            hasChanged = True
+
+        while hasChanged:
+            hasChanged = False
+            for lhs in self.grammar.P.keys():
+                productionsContainingNonTerminal = self.isContainedInRhs(lhs)
+                currentSet = deepcopy(self.FOLLOW[lhs])
+
+                for production_key, p in productionsContainingNonTerminal.items():
+                    for production in p:
+                        copyCurrentSet = deepcopy(currentSet)
+                        for symbol in production:
+                            if symbol == lhs:
+                                if symbol != production[len(production) - 1]:
+                                    next_symbol = production[production.index(symbol) + 1]
+                                    if next_symbol in self.grammar.Sigma:
+                                        currentSet.add(next_symbol)
+                                        break
+                                    # elif next_symbol == production[len(production) - 1]: ceva ceva to be done daca e ultimu in secventa
+                                    #     pass
+                                        # currentSet = currentSet.union(self.FOLLOW[production_key])
+                                    else:
+                                        currentSet = currentSet.union(self.FIRST[next_symbol])
+                                        if 'eps' in self.FIRST[next_symbol]:
+                                            currentSet.remove('eps')
+                                            currentSet = currentSet.union(self.FOLLOW[production_key])
+                                else:
+                                    currentSet = currentSet.union(self.FOLLOW[production_key])
+
+                        if len(self.FOLLOW[lhs]) != len(currentSet):
+                            self.FOLLOW[lhs] = currentSet
+                            hasChanged = True
+
+
+    def isContainedInRhs(self, nonTerminal):
+        productionsContainingNonTerminal = {}
+        for lhs in self.grammar.P.keys():
+            productions = self.grammar.P[lhs]
+            for production in productions:
+                productionList = production[0]
+                for symbol in productionList:
+                    if symbol == nonTerminal:
+                        if lhs in productionsContainingNonTerminal.keys():
+                            productionsContainingNonTerminal[lhs].append(productionList)
+                        else:
+                            productionsContainingNonTerminal[lhs] = [productionList]
+
+        return productionsContainingNonTerminal
+
 
 if __name__ == "__main__":
     grammar = Grammar("g3.txt")
     parser = LL1Parser(grammar)
     parser.constructFirst()
+    print(parser.FIRST)
+    print(parser.isContainedInRhs("E'"))
+    print(parser.FOLLOW)
